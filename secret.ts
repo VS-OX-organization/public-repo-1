c60295d20c03f11d11af16aeb9279d0a504ff611
victoria1196 <victoria.sidorchik@ox.security>
2026-06-12 11:23:29 +0300

    Update short_pass value to a longer string

--- secret.ts
+++ secret.ts
@@ -1,2 +1,2 @@
-export const short_pass = "qwerty1234567890123456789012";
+export const short_pass = "qwerty12345678901234567890123";
 export const GH_PAT_ORIG = "ghp_jrgoU8ts3fbszM5Ln2oH97FGiDX7IU20w4ZN";
